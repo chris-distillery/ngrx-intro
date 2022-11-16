052a8d82adb4f4cdf9428b4d909bd178169f31b5
Christian Algarin <christian.algarin@scorpion.co>
2022-11-16 04:12:15 -0300

    load basic angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { itemsReducer } from './state/reducers/items.reducers';
 import { ROOT_REDUCERS } from './state/app.state';
 import { EffectsModule } from '@ngrx/effects';
 import { ItemsEffects } from './state/effects/items.effects';
@@ -15,6 +15,7 @@ import { ItemsEffects } from './state/effects/items.effects';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot(ROOT_REDUCERS),
--- src/app/modules/show-case/components/ui-block-item/ui-block-item.component.ts
+++ src/app/modules/show-case/components/ui-block-item/ui-block-item.component.ts
@@ -1,5 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ShowCaseService } from '@modules/show-case/services/show-case.service';
+import {
+  animate,
+  animateChild,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from '../../../../state/app.state';
@@ -9,6 +17,21 @@ import { selectListItems } from '../../../../state/selectors/items.selectors';
   selector: 'app-ui-block-item',
   templateUrl: './ui-block-item.component.html',
   styleUrls: ['./ui-block-item.component.scss'],
+  animations: [
+    trigger('list', [
+      transition('* => *', [query(':enter', stagger(250, animateChild()), {optional: true})]),
+    ]),
+    trigger('animate', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-10px)' }),
+        animate('250ms', style({ opacity: 1, transform: 'translateX(0px)' }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('250ms', style({ opacity: 0, transform: 'translateX(10px)' }))
+      ])
+    ])
+  ],
 })
 export class UiBlockItemComponent implements OnInit {
   items$: Observable<any> = new Observable();
